ea053874ab3c71b2ae2e6c80943e7092f09d8e0e
unknown <gefreekweyu@gmail.com>
2024-10-10 03:42:36 +0300

    1.6to1.14: 1.6: unicafe step 1 compact functions

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,15 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState([])
-
-
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    //setTotal(good)
-  }
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    //setTotal(neutral)
-  }
-
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    //setTotal(bad)
-  }
+ 
 
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
       
       <h1>Statistics</h1>
       <p>good {good}</p>
