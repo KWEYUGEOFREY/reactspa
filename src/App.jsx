e1fb14af0bb9b562b64dcd8fc49718f3eca0cfc7
unknown <gefreekweyu@gmail.com>
2024-10-10 03:59:35 +0300

    1.6to1.14: 1.7: unicafe step 2 more statistics

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,14 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      
+
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All: {good + neutral + bad}</p>
+      <p>Average: {(good - bad) / (good + neutral + bad)|| 0}%</p>
+      <p>Positive: {(good / (good + neutral + bad)) * 100 || 0}%</p>
     </div>
   )
 }
