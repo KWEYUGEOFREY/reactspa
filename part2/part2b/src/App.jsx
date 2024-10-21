3bb19df6bccf7bfcc13481bef923f727b7db3114
unknown <gefreekweyu@gmail.com>
2024-10-21 04:16:59 +0300

    part b forms 2.6

--- part2/part2b/src/App.jsx
+++ part2/part2b/src/App.jsx
@@ -1,87 +1,50 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <h3>Total of {sum} exercises</h3>
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-// Course component to handle individual course data
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={totalExercises} />
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [newName, setNewName] = useState('')
 
-  // Map over all courses and render each Course component
+  const addName = (event)=>{
+    event.preventDefault()
+    const personsObject ={
+      name: newName
+  
+    }
+    setPersons(persons.concat(personsObject))
+    setNewName('')
+
+  }
+
+  const handleNameChange = (event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    
+
+  }
+
   return (
     <div>
-      <h1>Curriculum of Web Dev</h1>
-      {courses.map(course => (
-        <Course key={course.id} course={course} />
-      ))}
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addName}>
+        
+        
+        <div>
+          name: <input value={newName} onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+      {persons.map(person => 
+          <li>
+            {person.name}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
