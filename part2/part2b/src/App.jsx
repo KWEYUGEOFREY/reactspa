f64c18449ee533ac6ca2b12efe6b23b9894172a1
unknown <gefreekweyu@gmail.com>
2024-10-21 04:40:23 +0300

    part b forms 2.7

--- part2/part2b/src/App.jsx
+++ part2/part2b/src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('sample contact name')
 
   const addName = (event)=>{
     event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert('${newName} is already added to phonebook')
+    } 
+    else {
     const personsObject ={
       name: newName
   
     }
     setPersons(persons.concat(personsObject))
     setNewName('')
+   }
 
   }
 
@@ -39,8 +44,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person => 
-          <li>
+      {persons.map((person, i) => 
+          <li key ={i}>
             {person.name}
           </li>
         )}
